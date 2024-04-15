cadc590d6cb2d5063c3df84eafe593888b2de423
ADMIN-PC024\admin <jin.harry97@gmail.com>
2024-04-15 12:21:16 +0900

    4/15 - printf (%d, %s, %n, %f)

---
+++ Day_0415/src/Exam_08.java
@@ -0,0 +1,19 @@
+
+public class Exam_08 {
+
+	public static void main(String[] args) {
+		/* printf("문자열 %변환기호", 출력할 값)
+		 	%d(정수), %f(실수), %s(문자열), %n(newLine)
+		 	*/
+		
+		System.out.printf("Hello World\n");
+		System.out.printf("Hello World%n");
+		
+		System.out.printf("문자열 : %s\n", "Hello World");
+		System.out.printf("정수 : %d\n", 100);
+		System.out.printf("실수 : %f\n", 3.14);
+		
+		System.out.printf("국어 = %d, 영어 = %d, 수학 = %d", 90, 70, 80);
+	}
+
+}
