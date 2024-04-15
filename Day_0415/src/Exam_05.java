f59e37088cc649c4e07ca917c206134497841738
ADMIN-PC024\admin <jin.harry97@gmail.com>
2024-04-15 11:39:57 +0900

    4/15 - 문자열, 숫자, 문자의 결합

---
+++ Day_0415/src/Exam_05.java
@@ -0,0 +1,16 @@
+
+public class Exam_05 {
+
+	public static void main(String[] args) {
+		// 문자열과 숫자, 문자 결합
+		
+		System.out.println("이름 = " + "김학생");
+		System.out.println("국어 = 			" + 50);
+		// => "" 안의 공백만큼 춫력 시 공백 표시
+		System.out.println("영어 = " + 70);
+		System.out.println("수학 = " + 80);
+		System.out.println("총점 = " + 50 + 70 + 80);
+		System.out.println("총점 = " + (50 + 70 + 80));
+	}
+
+}
