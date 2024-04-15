bfda6f306f1c3f09a5b9394eef45f43d850d8586
ADMIN-PC024\admin <jin.harry97@gmail.com>
2024-04-15 11:19:14 +0900

    4/15 - 기본 (print, printf, println)

---
+++ Day_0415/src/Exam_01.java
@@ -0,0 +1,10 @@
+
+public class Exam_01 {
+
+	public static void main(String[] args) {
+		System.out.print("Hello World 3");
+		System.out.println("Hello World 2");
+		System.out.printf("Hello World 1");
+	}
+
+}
