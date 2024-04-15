64abde8f10c119479481b0eb431014483965129a
ADMIN-PC024\admin <jin.harry97@gmail.com>
2024-04-15 12:11:20 +0900

    4/15 - 예제

---
+++ Day_0415/src/Exam_07.java
@@ -0,0 +1,18 @@
+
+public class Exam_07 {
+
+	public static void main(String[] args) {
+		/*
+		 이름    국어    영어    수학   // println() 로 출력
+		 김학생   90    80     80    // print() 로 출력
+		 이학생   70    85     90
+		 강학생	100	  85     100
+		  */
+		
+		System.out.println("이름\t국어\t영어\t수학");
+		System.out.print("김학생\t" + 90 + "\t" + 80 + "\t" + 80 + "\n");
+		System.out.print("이학생\t" + 70 + "\t" + 85 + "\t" + 90 + "\n");
+		System.out.print("강학생\t" + 100 + "\t" + 85 + "\t" + 100);
+	}
+
+}
