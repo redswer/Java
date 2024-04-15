5b6d31cb502c15580e3621bcfeacef0a6a14d499
ADMIN-PC024\admin <jin.harry97@gmail.com>
2024-04-15 11:21:50 +0900

    4/15 - 줄바꿈 (\n, 빈 sysout)

---
+++ Day_0415/src/Exam_02.java
@@ -0,0 +1,14 @@
+
+public class Exam_02 {
+
+	public static void main(String[] args) {
+		// 결과 출력 후 줄바꿈
+		System.out.println("\n");
+		System.out.println();
+		
+		System.out.println("Hello\n");
+		System.out.println("Hello" + "\n");
+		System.out.println("Hello");
+	}
+
+}
