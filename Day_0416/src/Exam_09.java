728a1ddb5060f715f452beaa466a2b22a7f276c9
ADMIN-PC024\admin <jin.harry97@gmail.com>
2024-04-16 12:48:03 +0900

    4/16 - char 타입에 정수 대입

---
+++ Day_0416/src/Exam_09.java
@@ -0,0 +1,17 @@
+
+public class Exam_09 {
+
+	public static void main(String[] args) {
+		char ch = 'A';
+		char ch2 = 66;
+		char ch3 = 'B' + 1;
+		
+		System.out.println(ch);
+		System.out.println(ch2);
+		
+		System.out.printf("ch = %c\n", ch );
+		System.out.printf("ch2 = %c\n", ch2);
+		System.out.printf("ch3 = %c", ch3);
+	}
+
+}
