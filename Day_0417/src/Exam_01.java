bb1e4a6372b48d2bcd1cda75d1f02fa5392f39ea
ADMIN-PC024\admin <jin.harry97@gmail.com>
2024-04-17 09:50:41 +0900

    4/17 - 연산자 (a++ / ++a)

---
+++ Day_0417/src/Exam_01.java
@@ -0,0 +1,18 @@
+
+public class Exam_01 {
+
+	public static void main(String[] args) {
+		int a = 5;
+		System.out.println("a = " + a);
+		
+		a++;
+		System.out.println("a = " + a);
+		
+		++a;
+		System.out.println("a = " + a);
+		
+		System.out.println("a = " + a++);		
+		System.out.println("a = " + ++a);	
+	}
+
+}
