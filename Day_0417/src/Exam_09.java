e001d1a4156afd70af0c8330ed73abd94c777549
ADMIN-PC024\admin <jin.harry97@gmail.com>
2024-04-17 12:18:14 +0900

    4/17 - shift 연산

---
+++ Day_0417/src/Exam_09.java
@@ -0,0 +1,23 @@
+
+public class Exam_09 {
+
+	public static void main(String[] args) {
+		// shift 연산자 (<<, >>)
+		
+		int a = 1;
+		
+		System.out.println(a);
+		
+		int b = a << 1; // => a * 2의 1승
+		System.out.println(b);
+		b = a << 2; // => a * 2의 2승
+		System.out.println(b);
+		b = a << 3;  // => a * 2의 3승
+		System.out.println(b);
+		b = a << 4; // => a * 2의 4승
+		System.out.println(b);
+		b = a << 5;
+		System.out.println(b);
+	}
+
+}
