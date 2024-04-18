410f564c520a918c3f3d1554dccc014a02c45f43
ADMIN-PC024\admin <jin.harry97@gmail.com>
2024-04-18 09:38:28 +0900

    4/18 - 문자 + 숫자 (+형변환)

---
+++ Day_0418/src/Exam_01.java
@@ -0,0 +1,15 @@
+
+public class Exam_01 {
+
+	public static void main(String[] args) {
+		char ch = 'A' + 1; // 'B'
+		int a = 'A' + 1; // '66'
+		
+		System.out.println(ch);
+		System.out.println(a);
+		
+		System.out.println((int) ch);
+		System.out.println((char) a);
+	}
+
+}
