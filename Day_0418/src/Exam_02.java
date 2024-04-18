1bb14d806cc0fa94b045a28f9a968921f93b2a88
ADMIN-PC024\admin <jin.harry97@gmail.com>
2024-04-18 09:49:41 +0900

    4/18 - 관계연산자 / 상등연산자

---
+++ Day_0418/src/Exam_02.java
@@ -0,0 +1,17 @@
+
+public class Exam_02 {
+
+	public static void main(String[] args) {
+		int a = 55, b = 33;
+		
+		System.out.println(a + " >= " + b + " : " + (a >= b));
+		System.out.println(a + " <= " + b + " : " + (a <= b));
+		
+		boolean flag = a == b;
+		System.out.println(a + " == " + b + " : " + flag);
+		
+		flag = a != b;
+		System.out.println(a + " != " + b + " : " + flag);
+	}
+
+}
