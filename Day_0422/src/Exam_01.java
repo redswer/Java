2cf17b21764af47b2e3b75c249021af8e8557e49
ADMIN-PC024\admin <jin.harry97@gmail.com>
2024-04-22 09:37:14 +0900

    4/22 - 조건문

---
+++ Day_0422/src/Exam_01.java
@@ -0,0 +1,14 @@
+
+public class Exam_01 {
+
+	public static void main(String[] args) {
+//		int a = 75;
+		int a = 80;
+		
+		if (a >= 80) {
+			System.out.println("Pass");
+		}
+		System.out.println("프로그램 종료");
+	}
+
+}
