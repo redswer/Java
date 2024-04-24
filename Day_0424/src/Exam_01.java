bc14ed9eacb06b44152e78bd900bd434e7d7c9e9
ADMIN-PC024\admin <jin.harry97@gmail.com>
2024-04-24 09:47:04 +0900

    4/24 - 반복문

---
+++ Day_0424/src/Exam_01.java
@@ -0,0 +1,26 @@
+
+public class Exam_01 {
+
+	public static void main(String[] args) {
+		// Hello World 문자를 5번 출력
+		
+		/* System.out.println("Hello World");
+		 * System.out.println("Hello World");
+		 * System.out.println("Hello World");
+		 * System.out.println("Hello World");
+		 * System.out.println("Hello World"); */
+		
+		for (int i = 0; i < 5; i++) {
+			System.out.println("Hello World");
+		}
+		
+		System.out.println("--------------------------------");
+		
+		int i = 0;
+		while(i < 5) {
+			System.out.println("Hello World");
+			i++;
+		}
+	}
+
+}
