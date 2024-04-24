57ebfd83e54deff8a155edc7fc7b0429eaf44053
ADMIN-PC024\admin <jin.harry97@gmail.com>
2024-04-24 12:33:03 +0900

    4/24 - 화폐 단위별 매수 구하기

---
+++ Alogorithm/src/forWhile/Ex13.java
@@ -0,0 +1,49 @@
+package forWhile;
+
+import java.util.Scanner;
+
+public class Ex13 {
+
+	public static void main(String[] args) {
+		/* 화폐 단위별 매수 구하기
+		 * 급여 : 176668
+		 * 50000 원 : 3
+		 * 10000 원 : 2
+		 * 5000 원 : 1
+		 * 1000 원 : 1
+		 * 500 원 : 1
+		 * 100 원 : 1
+		 * 50 원 : 1
+		 * 10 원 1
+		 * 5 원 : 1
+		 * 1 원 : 3
+		 * 
+		 * */
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("급여 : ");
+		int na = sc.nextInt();
+		
+		int dan = 50000;
+		int cnt = 0;
+		int mok = 0;
+		
+		while (dan != 0) {
+			if (cnt == 0) {
+				mok = na / dan;
+				na = na % dan;
+				System.out.println(dan + " 원 : " + mok);
+				dan /= 5;
+				cnt = 1;
+			} else if (cnt == 1) {
+				mok = na / dan;
+				na = na % dan;
+				System.out.println(dan + " 원 : " + mok);
+				dan /= 2;
+				cnt = 0;
+			}
+		}
+	}
+
+}
