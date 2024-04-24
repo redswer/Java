c9c50ac96f01ace18e9902d4f65e8a0a5689b25d
ADMIN-PC024\admin <jin.harry97@gmail.com>
2024-04-24 09:47:27 +0900

    4/24 - 반복문 (1~10까지 출력)

---
+++ Day_0424/src/Exam_02.java
@@ -0,0 +1,20 @@
+
+public class Exam_02 {
+
+	public static void main(String[] args) {
+		// 1 2 3 4 5 ... 10 출력
+		
+		for (int i = 1; i <= 10; i++) {
+			System.out.print(i + " ");
+		}
+		
+		System.out.println("\n----------------------------------");
+		
+		int i = 0;
+		while(i < 10) {
+			i++;
+			System.out.print(i + " ");
+		}
+	}
+
+}
