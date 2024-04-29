8094daf1711fb361809d714bd273d7050fe3b452
ADMIN-PC024\admin <jin.harry97@gmail.com>
2024-04-29 10:01:03 +0900

    4/29 - 피라미드

---
+++ Day_0429/src/Exam_05.java
@@ -0,0 +1,34 @@
+
+public class Exam_05 {
+
+	public static void main(String[] args) {
+		for (int i = 1; i <= 5; i++) {
+			for (int j = 1; j <= i; j++) {
+				System.out.print("*");
+			}
+			
+			System.out.println();
+		}
+		
+		System.out.println("--------------------------");
+		
+		for (int i = 1; i <= 5; i++) {
+			for (int j = 1; j <= 6 - i; j++) {
+				System.out.print("*");
+			}
+			
+			System.out.println();
+		}
+		
+		System.out.println("--------------------------");
+		
+		for (int i = 5; i >= 1; i--) {
+			for (int j = 5; j >= i; j--) {
+				System.out.print("*");
+			}
+			
+			System.out.println();
+		}
+	}
+	
+}
