8d1409f443b00a97525ca517dfd509096f22e7aa
ADMIN-PC024\admin <jin.harry97@gmail.com>
2024-05-09 11:15:18 +0900

    5/9 - +while 문 사용

---
+++ Day_0509/src/Exam_03.java
@@ -0,0 +1,63 @@
+import java.util.Scanner;
+
+public class Exam_03 {
+
+	public static void main(String[] args) {
+		/* 1. 1차원 배열 100개를 선언하여 키보드로 점수를 입력받아 배열에 저장
+		 * 	단, 입력자료는 최대 100개를 넘지 않으며, 입력값에 -99가 있을 경우 종료
+		 * 
+		 * 2. 배열에 저장된 데이터를 출력
+		 *  단, 입력자료의 수가 10 이상일 경우 한 라인에 10개씩 출력
+		 *  
+		 * 3. 합계, 평균, 최대값, 최소값 출력
+		 * 
+		 * 4. while 문 사용
+		 * */
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int score[] = new int[100];
+		int cnt = 0;
+		
+		while (cnt <= 100) {
+			System.out.printf("data %d : ", cnt + 1);
+			score[cnt] = sc.nextInt();
+			
+			if (score[cnt] == -99) {
+				break;
+			}
+			cnt++;
+		}
+		
+		int max = score[0];
+		int min = score[0];
+		int sum = 0;
+		double avg = 0;
+		
+		int i = 0;
+		System.out.print("Input Data : ");
+		while(i < cnt) {
+			System.out.printf("%d ", score[i]);
+			if (i > 0 && (i + 1) % 10 == 0) {
+				System.out.println();
+			}
+			
+			if (score[i] > max) {
+				max = score[i];
+			} else if (score[i] < min) {
+				min = score[i];
+			}
+			sum += score[i];
+			i++;
+		}
+		
+		avg = (int)(sum / (double) cnt * 100 + 0.5) / 100.0;
+		
+		System.out.println();
+		System.out.println("합계 : " + sum);
+		System.out.println("평균 : " + avg);
+		System.out.println("최대값 : " + max);
+		System.out.println("최소값 : " + min);
+	}
+
+}
