a05caf4c862681ab9fe86527c8abef2fec2e54fc
ADMIN-PC024\admin <jin.harry97@gmail.com>
2024-05-21 10:30:27 +0900

    5/21 - 예제

---
+++ Day_0521/src/Exam_08.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+
+public class Exam_08 {
+	static int[] makeArray() {
+		int[] arr = {1, 2, 3, 4, 5};
+		
+		return arr;
+	}
+	public static void main(String[] args) {
+		int a[] = makeArray();
+		
+		System.out.println(Arrays.toString(a));
+	}
+
+}
