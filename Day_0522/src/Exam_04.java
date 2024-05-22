987034a8b813768e23b2fae7ea771d941d57cee4
ADMIN-PC024\admin <jin.harry97@gmail.com>
2024-05-22 10:05:40 +0900

    5/22 - (입력부분 메서드로 변경)

---
+++ Day_0522/src/Exam_04.java
@@ -0,0 +1,60 @@
+import java.util.Scanner;
+
+public class Exam_04 {
+	static int insert(int[] score, Scanner sc) {
+		int cnt;
+		for (cnt = 0; cnt < 10 ; cnt++) {
+			System.out.print("자료입력 : ");
+			score[cnt] = sc.nextInt();
+			
+			if (score[cnt] == -99) {
+				break;
+			}
+		}
+		return cnt;
+	}
+	
+	static void print(int[] score, int cnt, String str) {
+		System.out.print(str);
+		for (int i = 0; i < cnt; i++) {
+			System.out.printf("%3d", score[i]);
+		}
+	}
+	
+	static int[] sort(int[] score, int cnt) {
+		for (int i = 0; i < cnt - 1; i++) {
+			for (int j = i + 1; j < cnt; j++) {
+				if (score[i] > score[j]) {
+					int tmp = score[i];
+					score[i] = score[j];
+					score[j] = tmp;
+				}
+			}
+		}
+		
+		return score;
+	}
+
+	public static void main(String[] args) {
+		// 점수 최대 10개 입력, 오름차순 정렬 후 출력
+		// 입력자료에 -99가 있을 경우 종료
+		
+		// 입력
+		int score[] = new int[10];
+		Scanner sc = new Scanner(System.in);
+		
+		int cnt = insert(score, sc);
+		System.out.println();
+		
+		// 입력자료 출력
+		print(score, cnt, "입력자료 : ");
+		System.out.println();
+		
+		// 오름차순 정렬
+		sort(score, cnt);
+		
+		// 정렬 자료 출력
+		print(score, cnt, "정렬자료 : ");
+	}
+
+}
