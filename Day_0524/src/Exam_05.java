0bfe61e718dd4df0f404bb5764ad764c2ac5d5b1
ADMIN-PC024\admin <jin.harry97@gmail.com>
2024-05-24 09:48:19 +0900

    5/24 - 예제 (팩토리얼 (!))

---
+++ Day_0524/src/Exam_05.java
@@ -0,0 +1,17 @@
+
+public class Exam_05 {
+	static int func(int num) {
+		if (num == 1) {
+			return 1;
+		}
+		
+		int t = num * func(num - 1);
+		System.out.println(t);
+		return t;
+	}
+	public static void main(String[] args) {
+		System.out.println(func(5));
+		// => 5! = 5 * 4 * 3 * 2 * 1
+	}
+
+}
