e38188fa3857d2d3074a75bc633bd25661f1b966
ADMIN-PC024\admin <jin.harry97@gmail.com>
2024-05-27 10:13:28 +0900

    5/27 - 사용자 정의 클래스 (추상 클래스)

---
+++ Day_0527/src/Exam_03.java
@@ -0,0 +1,14 @@
+// 사용자 정의 클래스 (설계도)
+
+public class Exam_03 {
+
+	public static void main(String[] args) {
+		Point p = new Point(); // => p : 참조변수
+		p.line();
+		
+		p.x = 1;
+		p.y = 100;
+		p.line();
+	}
+
+}
