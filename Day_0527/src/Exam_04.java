8b3e762f9e36eb36a25a1e57be80933f8d66956e
ADMIN-PC024\admin <jin.harry97@gmail.com>
2024-05-27 10:19:54 +0900

    5/27 - 계산기

--- Day_0527/src/Exam_04.java
+++ Day_0527/src/Exam_04.java
@@ -2,8 +2,12 @@
 public class Exam_04 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Account a = new Account();
+		
+		a.add(1, 10);
+		a.sub(1, 10);
+		a.mul(1, 10);
+		a.div(1, 10);
 	}
 
 }
---
+++ Day_0527/src/Point.java
@@ -0,0 +1,11 @@
+// x, y 좌표를 이용하여 선 긋기
+public class Point {
+	// 속성
+	int x;
+	int y;
+	
+	// 기능
+	void line() {
+		System.out.println(x + "에서 " + y + "까지 선을 긋다");
+	}
+}
