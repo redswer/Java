55bbe80deceb6380d560bacc1ea3cd5b99762779
ADMIN-PC024\admin <jin.harry97@gmail.com>
2024-05-29 10:19:46 +0900

    5/29 - 예제 (사원등록)

---
+++ Day_0529/src/Exam_04.java
@@ -0,0 +1,62 @@
+import java.util.Scanner;
+
+class Sawon {
+	int bun;
+	String name;
+	String gender;
+	int age;
+	
+	Sawon() {
+	}
+	
+	Sawon(int bun, String name, String gender, int age) {
+		this.bun = bun;
+		this.name = name;
+		this.gender = gender;
+		this.age = age;
+	}
+	
+	void print() {
+		String g = "여자";
+		
+		if (gender.equals("m") || gender.equals("M")) {
+			g = "남자";
+		}
+		
+		System.out.printf("%d\t%s\t%s\t%d\n", bun, name, g, age);
+	}
+}
+public class Exam_04 {
+
+	public static void main(String[] args) {
+		// 사원 최대 10명, 번호가 0이면 출력 후 종료
+		
+		Scanner sc = new Scanner(System.in);
+		Sawon s[] = new Sawon[10];
+		
+		int cnt = 0;
+		
+		while(cnt < 10) {
+			System.out.print("사원등록 : ");
+			int bun = sc.nextInt();
+			
+			if (bun == 0) {
+				break;
+			}
+			
+			String name = sc.next();
+			String gender = sc.next();
+			int age = sc.nextInt();
+			
+			s[cnt] = new Sawon(bun, name, gender, age);
+			
+			cnt++;
+		}
+		
+		System.out.println("번호\t이름\t성별\t나이");
+		for (int i = 0; i < cnt; i++) {
+			s[i].print();
+		}
+	}
+
+}
