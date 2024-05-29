e0793c3f668879a7499182595ab30b7042051194
ADMIN-PC024\admin <jin.harry97@gmail.com>
2024-05-29 10:08:24 +0900

    Revert "5/29 - 객체 배열"
    This reverts commit 3efd4ffc95bcdee81cc9c97c1e28f5a13154ac85.

--- Day_0529/src/Exam_03.java
+++ Day_0529/src/Exam_03.java
@@ -1,4 +1,3 @@
-
 class Person {
 	int bun;
 	String name;
@@ -11,25 +10,8 @@ class Person {
 		this.name = name;
 		this.age = age;
 	}
-	
-	void print() {
-		System.out.println(bun + "\t" + name + "\t" + age);
-	}
 }
 
 public class Exam_03 {
-	public static void main(String[] args) {
-		Person p[] = new Person[10];
-		
-		p[0] = new Person(1481, "김", 24);
-		p[1] = new Person(1237, "이", 27);
-		p[2] = new Person(2437, "나", 35);
-		p[3] = new Person(1458, "박", 57);
-		
-		System.out.println("주민번호\t이름\t나이");
-		for (int i = 0; i < 4; i++) {
-			System.out.printf("%d\t%s\t%d\n", p[i].bun, p[i].name, p[i].age);
-		}
-	}
-	
+
 }
