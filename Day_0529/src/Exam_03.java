2a8fe976a0d1306baf03c0cb89e59915740cbb44
ADMIN-PC024\admin <jin.harry97@gmail.com>
2024-05-29 09:46:18 +0900

    5/29 - 복습(생성자)

---
+++ Day_0529/src/Exam_03.java
@@ -0,0 +1,17 @@
+class Person {
+	int bun;
+	String name;
+	int age;
+	
+	Person () {}
+	
+	Person(int bun, String name, int age) {
+		this.bun = bun;
+		this.name = name;
+		this.age = age;
+	}
+}
+
+public class Exam_03 {
+
+}
