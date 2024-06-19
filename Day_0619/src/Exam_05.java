fa066beb7db9ad5c093e0272d991a89823c9dcee
ADMIN-PC024\admin <jin.harry97@gmail.com>
2024-06-19 10:54:02 +0900

    6/19 - Math

---
+++ Day_0619/src/Exam_05.java
@@ -0,0 +1,28 @@
+
+public class Exam_05 {
+
+	public static void main(String[] args) {
+		// 절대값 : Math.abs()
+		System.out.println("Math.abs(7-10) : " + Math.abs(7-10));
+		
+		// max / min
+		System.out.println("Math.max(5, 7) : " + Math.max(5, 7));
+		System.out.println("Math.min(5, 7) : " + Math.min(5, 7));
+		
+		// 루트
+		System.out.println("Math.sqrt(2) : " + Math.sqrt(2));
+		
+		// 제곱
+		System.out.println("Math.pow(2, 3) : " + Math.pow(2, 3));
+		
+		// 반올림
+		System.out.println("Math.round(3.54321) : " + Math.round(3.54321));
+		
+		// 올림
+		System.out.println("Math.ceil(7/2) : " + Math.ceil(7 / 2.0));
+		
+		// 버림
+		System.out.println("Math.floor(7/2) : " + Math.floor(7 / 2.0));
+	}
+
+}
