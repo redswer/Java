d33d09dc6b136b123fd8777e37ab48a154f643bb
ADMIN-PC024\admin <jin.harry97@gmail.com>
2024-06-20 09:57:36 +0900

    6/20 - 과목별 총점 / 평균 추가

--- Day_0620/src/com/jslhrd/aaa/Exam_01.java
+++ Day_0620/src/com/jslhrd/aaa/Exam_01.java
@@ -7,8 +7,11 @@ public class Exam_01 {
 	public static void main(String[] args) throws Exception {
 		Scanner sc = new Scanner(new File("src/Student.txt"));
 		int cnt = 0;
+		int k_tot = 0;
+		int e_tot = 0;
+		int m_tot = 0;
 		
-		System.out.println("번호\t이름\t국어\t영어\t수학\t총점\t평균");
+		System.out.println("번호\t이름\t국어\t영어\t수학\t총점\t평균\t등급");
 		while(sc.hasNextLine()) {
 			String str = sc.nextLine();
 			String[] info = str.split(",");
@@ -19,15 +22,38 @@ public class Exam_01 {
 			int eng = Integer.parseInt(info[3]);
 			int math = Integer.parseInt(info[4]);
 			
+			k_tot += kor;
+			e_tot += eng;
+			m_tot += math;
+			
 			cnt++;
 			
 			int tot = kor + eng + math;
 			double avg = tot / 3.0;
+			char grade = 'F';
 			
-			System.out.printf("%s\t%s\t%d\t%d\t%d\t%d\t%.2f\n", bun, name, kor, eng, math, tot, avg);
+			switch ((int)avg / 10) {
+			case 10:
+			case 9:
+				grade = 'A';
+				break;
+				
+			case 8:
+				grade = 'B';
+				
+			case 7:
+				grade = 'C';
+				
+			case 6:
+				grade = 'D';
+			}
+			
+			System.out.printf("%s\t%s\t%d\t%d\t%d\t%d\t%.1f\t%s\n", bun, name, kor, eng, math, tot, avg, grade);
 		}
 		
-		System.out.println("학생수 : " + cnt);
+		double tot_avg = (k_tot + e_tot + m_tot) / (double)(cnt * 3);
+		
+		System.out.printf("전체 : \t%d명\t%d\t%d\t%d\t%d\t%.2f\n", cnt, k_tot, e_tot, m_tot, (k_tot + e_tot + m_tot), tot_avg);
 	}
 
 }
