b7ad62cf9e3adf6b616268c2f7263d54c93ab7c5
ADMIN-PC024\admin <jin.harry97@gmail.com>
2024-07-01 11:16:05 +0900

    7/1 - 에제 (List 사용)

---
+++ Day_0701/src/com/jslhrd/aaa/Student.java
@@ -0,0 +1,112 @@
+package com.jslhrd.aaa;
+
+public class Student {
+	private String bun;
+	private String name;
+	private int kor;
+	private int eng;
+	private int math;
+	private int tot;
+	private double avg;
+	private char grade;
+	
+	public Student() {}
+	
+	public Student(String bun, String name, int kor, int eng, int math) {
+		this.bun = bun;
+		this.name = name;
+		this.kor = kor;
+		this.eng = eng;
+		this.math = math;
+		
+		tot = kor + eng + math;
+		avg = tot / 3.0;
+		
+		switch ((int)avg / 10) {
+		case 10:
+		case 9:	
+			grade = 'A';
+			break;
+
+		case 8:
+			grade = 'B';
+			break;
+			
+		case 7:
+			grade = 'C';
+			break;
+			
+		case 6:
+			grade = 'D';
+			break;
+			
+		default:
+			grade = 'F';
+			break;
+		}
+	}
+
+	public String getBun() {
+		return bun;
+	}
+
+	public void setBun(String bun) {
+		this.bun = bun;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getKor() {
+		return kor;
+	}
+
+	public void setKor(int kor) {
+		this.kor = kor;
+	}
+
+	public int getEng() {
+		return eng;
+	}
+
+	public void setEng(int eng) {
+		this.eng = eng;
+	}
+
+	public int getMath() {
+		return math;
+	}
+
+	public void setMath(int math) {
+		this.math = math;
+	}
+
+	public int getTot() {
+		return tot;
+	}
+
+	public void setTot(int tot) {
+		this.tot = tot;
+	}
+
+	public double getAvg() {
+		return avg;
+	}
+
+	public void setAvg(double avg) {
+		this.avg = avg;
+	}
+
+	public char getGrade() {
+		return grade;
+	}
+
+	public void setGrade(char grade) {
+		this.grade = grade;
+	}
+}
---
+++ Day_0701/src/com/jslhrd/aaa/StudentListExam.java
@@ -0,0 +1,60 @@
+package com.jslhrd.aaa;
+
+import java.io.File;
+import java.util.*;
+
+public class StudentListExam {
+	/* [출력형식]
+	 * 번호	이름		국어		영어		수학		총점		평균		등급
+	 * 1	김학생	88		79		90		256		85.3	B
+	 * ...
+	 * 
+	 * 전체총점 : 		xxx		xxx		xxx
+	 * 전체평균 : 		xx.x	xx.x	xx.x
+	 * 
+	 * [처리조건]
+	 * 등급은 90 이상이면 A, 80 이상이면 B, 70 이상이면 C, 60 이상이면 D, 60 미만이면 F
+	 * */
+	public static void main(String[] args) throws Exception {
+		List<Student> list = new ArrayList<>();
+		Scanner sc = new Scanner(new File("src/data.txt"));
+		
+		while(sc.hasNext()) {
+			String student = sc.nextLine();
+			String s[] = student.split(",");
+			
+			String bun = s[0];
+			String name = s[1];
+			int kor = Integer.parseInt(s[2]);
+			int eng = Integer.parseInt(s[3]);
+			int math = Integer.parseInt(s[4]);
+			
+			list.add(new Student(bun, name, kor, eng, math));
+		}
+		
+		int kor_tot = 0;
+		int eng_tot = 0;
+		int math_tot = 0;
+		
+		System.out.println("번호\t이름\t국어\t영어\t수학\t총점\t평균\t등급");
+		for (int i = 0; i < list.size(); i++) {
+			System.out.print(list.get(i).getBun() + "\t");
+			System.out.print(list.get(i).getName() + "\t");
+			System.out.print(list.get(i).getKor() + "\t");
+			System.out.print(list.get(i).getEng() + "\t");
+			System.out.print(list.get(i).getMath() + "\t");
+			System.out.print(list.get(i).getTot() + "\t");
+			System.out.printf("%.1f\t", list.get(i).getAvg());
+			System.out.print(list.get(i).getGrade() + "\n");
+			
+			kor_tot += list.get(i).getKor();
+			eng_tot += list.get(i).getEng();
+			math_tot += list.get(i).getMath();
+		}
+		
+		System.out.println();
+		System.out.printf("전체총점 :\t\t%d\t%d\t%d\n", kor_tot, eng_tot, math_tot);
+		System.out.printf("전체평균 :\t\t%.1f\t%.1f\t%.1f", (double)kor_tot / list.size(), (double)eng_tot / list.size(), (double)math_tot / list.size());
+	}
+
+}
