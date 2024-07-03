80910e11dcf4f339b27f064c1dac570b3af92533
ADMIN-PC024\admin <jin.harry97@gmail.com>
2024-07-03 10:24:50 +0900

    7/3 - PrintWriter

---
+++ Day_0703/src/com/jslhrd/aaa/Exam_05.java
@@ -0,0 +1,22 @@
+package com.jslhrd.aaa;
+
+import java.io.*;
+
+public class Exam_05 {
+
+	public static void main(String[] args) throws Exception {
+		File f = new File("src/output.txt");
+		
+		FileWriter fw = new FileWriter(f);
+		BufferedWriter bw = new BufferedWriter(fw);
+		PrintWriter pw = new PrintWriter(bw);
+		
+		pw.print("1. 파일에 쓰기");
+		pw.println();
+		pw.println("2. 파일에 쓰기");
+		pw.println("3. File Writer");
+		pw.println("4. 1234567890");
+		pw.close();
+	}
+
+}
